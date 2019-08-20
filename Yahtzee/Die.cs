6b36cdb1c77dc6bf16a682eae8a3e3711b606af6
Jim Doyle <indellibleink@gmail.com>
2019-08-19 21:46:57 -0400

    Add project files.

---
+++ Yahtzee/Die.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yahtzee
+{
+    class Die
+    {
+        public int Faces;
+        public int FaceUp;
+        public List<Die> dice = new List<Die>();
+       
+        public Die()
+        {
+            Random cast = new Random();
+            this.Faces = 6;
+            this.FaceUp = cast.Next(1, 6); 
+        }
+
+        public Die( int faceUp)
+        {
+ 
+            this.FaceUp = faceUp;
+        }
+
+        public List<Die> Dice(int count)
+        {
+            for (int cast = 1; cast < count; cast++)
+            {
+                Die die = new Die();
+                dice.Add(die); 
+            }
+            return dice;
+        }
+    }
+}
---
+++ Yahtzee/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yahtzee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die = new Die();
+           
+            List<Die> dice = die.Dice(6);
+
+            foreach (var cube in dice)
+                Console.WriteLine(cube.FaceUp);
+
+           // int index=dice.FindIndex(item=> item.FaceUp == )
+        }
+    }
+}
