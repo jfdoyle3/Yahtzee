9fa83376778e91450e67d2cb388358f2ab6702ab
Jim Doyle <indellibleink@gmail.com>
2019-09-04 00:01:12 -0400

    Working on User Input method

--- Yahtzee/Game.cs
+++ Yahtzee/Game.cs
@@ -10,18 +10,20 @@ namespace Yahtzee
         public List<Die> playerDice=new List<Die>();
         public void Play()
         {
-
             for (int rollDice = 0; rollDice < 5; rollDice++)
-                    playerDice.Add(new Die(6));
+                playerDice.Add(new Die(6));
+
+            string choice=UserInput();
+            playerDice[0].Roll();
 
 
-            for (int i = 0; i < 3; i++)
-            {
-
-                Roll();
-
-                Output();
-            }
+           // Output();
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Roll();
+            //    Output();
+            //    UserInput();  
+            //}
         }
         public void Roll()
         { 
@@ -29,6 +31,23 @@ namespace Yahtzee
               playerDice[rollnum].Roll();    
         }
         
+        public string UserInput()
+        {
+            Output();
+            Console.Write("Choice:  ");
+            for (int opt = 0; opt < playerDice.Count; opt++)
+            {
+                Console.Write("{0} ", opt);
+            }
+            Console.WriteLine("Choose a Die to Re-roll");
+            string choice = Console.ReadLine();
+
+            //Console.WriteLine("\n-----------------------------\n");
+            //Console.WriteLine("Roll Count: {0}", rollCount);
+
+            return choice;
+
+        }
         public void Output()
         {
             Console.Write("Roll:    ");
@@ -39,16 +58,7 @@ namespace Yahtzee
 
             }
             Console.WriteLine();
-            //Console.Write("Choice:  ");
-            //for (int opt = 0; opt < playerDice.Count; opt++)
-            //{
-            //    Console.Write("{0} ", opt);
-            //}
-
-
-
-            //Console.WriteLine("\n-----------------------------\n");
-            //Console.WriteLine("Roll Count: {0}", rollCount);
+            
         }
     }
 }
